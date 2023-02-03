c8fa3ce810d06943bc43bcf9e22c7ae06c0fea91
simonbjerkas <simon.bjerkas10@gmail.com>
2023-02-03 12:58:46 +0100

    Clearing out unesseccary files

---
+++ tsx/src/index.ts
@@ -0,0 +1,11 @@
+// 1) import React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// 2) Get a reference to to div with ID root
+
+// 3) Tell React to take control of that element
+
+// 4) Create a component
+
+// 5) Show the component on the screen
