cb748c49b619fb414e5f51aadac6eed59b782d8d
simonbjerkas <simon.bjerkas10@gmail.com>
2023-03-01 14:58:46 +0100

    Accordion completed

---
+++ comps/src/pages/AccordionPage.tsx
@@ -0,0 +1,28 @@
+import Accordion, { TAccordion } from '../components/Accordion';
+
+const AccordionPage = () => {
+  const items: TAccordion[] = [
+    {
+      id: 'sidfjdi',
+      label: 'Can I use React on a project?',
+      content:
+        'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
+    },
+    {
+      id: 'dsfbk',
+      label: 'Can I use Typescript on a project?',
+      content:
+        'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
+    },
+    {
+      id: 'dfubs',
+      label: 'Can I use CSS on a project?',
+      content:
+        'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
+    },
+  ];
+
+  return <Accordion items={items} />;
+};
+
+export default AccordionPage;
