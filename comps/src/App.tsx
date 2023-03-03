916816449a64d86c984bff92b925b499ae8c16cc
simonbjerkas <simon.bjerkas10@gmail.com>
2023-03-03 12:55:41 +0100

    Dropdown page moved from App to DropdownPage

--- comps/src/App.tsx
+++ comps/src/App.tsx
@@ -1,23 +1,5 @@
-import { useState } from 'react';
-import Dropdown, { TDropdown } from './components/Dropdown';
-
 const App = () => {
-  const [selection, setSelection] = useState<TDropdown | null>(null);
-
-  const handleSelect = (option: TDropdown): void => setSelection(option);
-
-  const options: TDropdown[] = [
-    { label: 'Red', value: 'red' },
-    { label: 'Blue', value: 'blue' },
-    { label: 'Green', value: 'green' },
-  ];
-
-  return (
-    <div className="flex">
-      <Dropdown options={options} value={selection} onChange={handleSelect} />
-      <Dropdown options={options} value={selection} onChange={handleSelect} />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
---
+++ comps/src/pages/DropdownPage.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import Dropdown, { TDropdown } from '../components/Dropdown';
+
+const DropdownPage = () => {
+  const [selection, setSelection] = useState<TDropdown | null>(null);
+
+  const handleSelect = (option: TDropdown): void => setSelection(option);
+
+  const options: TDropdown[] = [
+    { label: 'Red', value: 'red' },
+    { label: 'Blue', value: 'blue' },
+    { label: 'Green', value: 'green' },
+  ];
+
+  return (
+    <div className="flex">
+      <Dropdown options={options} value={selection} onChange={handleSelect} />
+      <Dropdown options={options} value={selection} onChange={handleSelect} />
+    </div>
+  );
+};
+
+export default DropdownPage;
